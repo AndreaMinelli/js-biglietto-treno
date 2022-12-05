939f83d565dd46bfee4b1d0841c5342d1d9e4f07
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-05 15:03:44 +0100

    Impostati passaggi logici

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('JS OK');
+/* 
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+*/
+
+/*
+0 - Impostare prezzo fisso a chilometro
+0a - Definire sconto per minorenni
+0b - Definire sconto per over 65
+1 - Richiedere numero chilometri
+2 - Richiedere età del passeggero
+3 - Calcolare prezzo a chilometro
+4 - Definire se si applica sconto
+5 - Applicare sconto se minorenne o over 65
+6 - Visualizzare prezzo finale con centesimi
+*/
